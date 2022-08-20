262da3931937009cce3e62d65717d315ad766913
Amal K K <kkamalkk1994@gmail.com>
2022-08-20 22:55:49 +0530

    Footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { getInfo } from "../redux/infoReducer"
+import { FaGlobe } from "react-icons/fa"
+import { FaFlickr } from "react-icons/fa"
+import { FaTwitter } from "react-icons/fa"
+import { FaGlobeAmericas } from "react-icons/fa"
 
 const Footer = () => {
   const dispatch = useDispatch()
@@ -8,25 +12,29 @@ const Footer = () => {
   const error = useSelector((state) => state.info.error)
   const loading = useSelector((state) => state.info.loading)
 
+  console.log("data footer:" + data)
+
   useEffect(() => {
     dispatch(getInfo())
   }, [dispatch])
   return (
-    <footer className="flex text-zinc-200 bg-slate-800 p-10 justify-around">
-      <div className="flex gap-5">
-        <p>{data.headquarters.address}</p>
-        <p>{data.headquarters.city}</p>
-        <p>{data.headquarters.state}</p>
+    <footer className="flex text-zinc-200 bg-slate-800 p-10 justify-between">
+      <div className="flex gap-1">
+        <FaGlobeAmericas className="self-center" />
+        <p>{data.headquarters?.address},</p>
+        <p>{data.headquarters?.city},</p>
+        <p>{data.headquarters?.state}</p>
       </div>
+      <p>&copy; SpaceX</p>
       <div className="flex gap-5">
-        <a href={data.links.website} target="_blank">
-          Website
+        <a href={data.links?.website} target="_blank">
+          <FaGlobe />
         </a>
-        <a href={data.links.flickr} target="_blank">
-          flickr
+        <a href={data.links?.flickr} target="_blank">
+          <FaFlickr />
         </a>
-        <a href={data.links.twitter} target="_blank">
-          twitter
+        <a href={data.links?.twitter} target="_blank">
+          <FaTwitter />
         </a>
       </div>
     </footer>
