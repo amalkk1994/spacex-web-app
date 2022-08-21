e0a1b0a43eef91c30e23c272c07c7e3d358a86d8
Amal K K <kkamalkk1994@gmail.com>
2022-08-21 23:45:44 +0530

    data enrichment mission and ship

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -1,8 +1,15 @@
 import React from "react"
+import FieldValueItem from "./FieldValueItem"
 
 const MissionCard = ({ missionItem }) => {
   console.log("mission card")
-  return <div>{missionItem.mission_name}</div>
+  return (
+    <div>
+      <FieldValueItem field="Mission Id" value={missionItem.mission_id} />
+      <FieldValueItem field="Mission Name" value={missionItem.mission_name} />
+      <FieldValueItem field="Description" value={missionItem.description} />
+    </div>
+  )
 }
 
 export default MissionCard
--- src/components/ShipCard.js
+++ src/components/ShipCard.js
@@ -1,8 +1,15 @@
 import React from "react"
+import FieldValueItem from "./FieldValueItem"
 
 const ShipCard = ({ shipItem }) => {
   console.log("ship card")
-  return <div>{shipItem.ship_name}</div>
+  return (
+    <div>
+      <FieldValueItem field="Ship Id" value={shipItem.ship_id} />
+      <FieldValueItem field="Ship Name" value={shipItem.ship_name} />
+      <FieldValueItem field="Ship Type" value={shipItem.ship_type} />
+    </div>
+  )
 }
 
 export default ShipCard
