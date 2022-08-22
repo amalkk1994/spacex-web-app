0bd8d43fe71ea1dd41f4439d75168ef062f94407
Amal K K <kkamalkk1994@gmail.com>
2022-08-22 08:20:52 +0530

    nav bar changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,10 +7,10 @@ const NavBar = () => {
   return (
     <nav className="bg-slate-800 py-3 px-4">
       <div className="flex justify-between">
-        <Link to="/">
-          <img src={logo} alt="logo" className="h-10" />
+        <Link to="/" className="sm:self-center">
+          <img src={logo} alt="logo" className="h-10 sm:h-auto" />
         </Link>
-        <ul className="flex gap-12 items-center mr-20">
+        <ul className="flex gap-12 items-center mr-20 sm:gap-6 sm:mr-5">
           <NavItem link="/launches" label="Launch" />
           <NavItem link="/missions" label="Missions" />
           <NavItem link="/ships" label="Ships" />
